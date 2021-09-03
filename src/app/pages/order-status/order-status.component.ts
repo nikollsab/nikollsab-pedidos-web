e7a52dee5abdbea9d8c8770533ead15c145e4f57
unknown <vector@L0003863.vectoritcgroup.com>
2021-09-03 17:18:08 -0500

    agregando validacion de errores

--- src/app/pages/order-status/order-status.component.ts
+++ src/app/pages/order-status/order-status.component.ts
@@ -10,7 +10,7 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./order-status.component.scss']
 })
 export class OrderStatusComponent implements OnInit {
-  @ViewChild('stepper', {static: true}) stepper: MatStepper;
+  @ViewChild('stepper', { static: true }) stepper: MatStepper;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   detallepedidos: any[] = [];
@@ -19,30 +19,31 @@ export class OrderStatusComponent implements OnInit {
   tracingI: any;
   idPedido: any
   currentStepMat: any;
-      isEditableStepControlOne: boolean;
-      isEditableStepControlTwo: boolean;
-      isEditableStepControlThree: boolean;
-      isEditableStepControlFour: boolean;
-      isEditableStepControlFive: boolean;
-      isCompletedtepControlOne: boolean
-      isCompletedtepControlTwo: boolean;
-      isCompletedtepControlThree: boolean;
-      isCompletedtepControlFour: boolean;
-      isCompletedtepControlFive: boolean;
+  isEditableStepControlOne: boolean;
+  isEditableStepControlTwo: boolean;
+  isEditableStepControlThree: boolean;
+  isEditableStepControlFour: boolean;
+  isEditableStepControlFive: boolean;
+  isCompletedtepControlOne: boolean
+  isCompletedtepControlTwo: boolean;
+  isCompletedtepControlThree: boolean;
+  isCompletedtepControlFour: boolean;
+  isCompletedtepControlFive: boolean;
+  isLoading: boolean = true;
+  mostrarError: boolean = false;
 
-  constructor(private _formBuilder: FormBuilder, private trackingPedido: ServicioService, private router: Router,private _router: ActivatedRoute) {
+  constructor(private _formBuilder: FormBuilder, private trackingPedido: ServicioService, private router: Router, private _router: ActivatedRoute) {
     console.log("username", this.idPedido)
     this.idPedido = this._router.snapshot.paramMap.get('id');
     this.trackingPedido.getDetallePedidoId(this.idPedido)
-    .subscribe(
-      async data => {
-        console.log(data);
-        this.detallepedidos = [...data];
-        console.log(data, 'Pedido por id');
-      }
-    );
+      .subscribe(
+        async data => {
+          console.log(data);
+          this.detallepedidos = [...data];
+        }
+      );
 
-   }
+  }
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
@@ -58,98 +59,94 @@ export class OrderStatusComponent implements OnInit {
       fourCtrl: ['', Validators.required]
     });
 
-    this.getTracing();
+    this.getTracking();
   }
 
-  getTracing() {
+  getTracking() {
 
-     this.trackingPedido.getPedidoId(this.idPedido).toPromise().then(data => {
+    this.trackingPedido.getPedidoId(this.idPedido).toPromise().then(data => {
       // async data => {
-        this.tracingI = data;
-        if(this.tracingI.estado.estadoId === 1){
-          this.isEditableStepControlOne = true;
-          this.isEditableStepControlTwo = false;
-          this.isEditableStepControlThree = false;
-          this.isEditableStepControlFour = false;
-          this.isCompletedtepControlOne = false;
-          this.isCompletedtepControlTwo = false;
-          this.isCompletedtepControlThree = false;
-          this.isCompletedtepControlFour = false;
-          this.currentStepMat = 0;
+      this.tracingI = data;
+      if (this.tracingI.estado.estadoId === 1) {
+        this.isEditableStepControlOne = true;
+        this.isEditableStepControlTwo = false;
+        this.isEditableStepControlThree = false;
+        this.isEditableStepControlFour = false;
+        this.isCompletedtepControlOne = false;
+        this.isCompletedtepControlTwo = false;
+        this.isCompletedtepControlThree = false;
+        this.isCompletedtepControlFour = false;
+        this.currentStepMat = 0;
 
-        }else if(this.tracingI.estado.estadoId === 2){
-          console.log("entre")
+      } else if (this.tracingI.estado.estadoId === 2) {
+        console.log("entre")
 
-          this.isEditableStepControlOne = false;
-          this.isEditableStepControlTwo = true;
-          this.isEditableStepControlThree = false;
-          this.isEditableStepControlFour = false;
-          this.isEditableStepControlFive = false;
-          this.isCompletedtepControlOne = true;
-          this.isCompletedtepControlTwo = false;
-          this.isCompletedtepControlThree = false;
-          this.isCompletedtepControlFour = false;
-          this.isCompletedtepControlFive = false;
-          this.currentStepMat = 1;
-          
+        this.isEditableStepControlOne = false;
+        this.isEditableStepControlTwo = true;
+        this.isEditableStepControlThree = false;
+        this.isEditableStepControlFour = false;
+        this.isEditableStepControlFive = false;
+        this.isCompletedtepControlOne = true;
+        this.isCompletedtepControlTwo = false;
+        this.isCompletedtepControlThree = false;
+        this.isCompletedtepControlFour = false;
+        this.isCompletedtepControlFive = false;
+        this.currentStepMat = 1;
 
-        }else if(this.tracingI.estado.estadoId === 3 ){
-          console.log("entre")
-          this.isEditableStepControlOne = false;
-          this.isEditableStepControlTwo = false;
-          this.isEditableStepControlThree = true;
-          this.isEditableStepControlFour = false;
-          this.isEditableStepControlFive = false;
-          this.isCompletedtepControlOne = true;
-          this.isCompletedtepControlTwo = true;
-          this.isCompletedtepControlThree = false;
-          this.isCompletedtepControlFour = false;
-          this.isCompletedtepControlFive = false;
-          this.currentStepMat = 2;
 
-        }else if(this.tracingI.estado.estadoId === 4 ){
-          console.log("entre")
-          this.isEditableStepControlOne = false;
-          this.isEditableStepControlTwo = false;
-          this.isEditableStepControlThree = false;
-          this.isEditableStepControlFour = true;
-          this.isEditableStepControlFive = false;
-          this.isCompletedtepControlOne = true;
-          this.isCompletedtepControlTwo = true;
-          this.isCompletedtepControlThree = true;
-          this.isCompletedtepControlFour = false;
-          this.isCompletedtepControlFive = false;
-          this.currentStepMat = 3;
+      } else if (this.tracingI.estado.estadoId === 3) {
+        console.log("entre")
+        this.isEditableStepControlOne = false;
+        this.isEditableStepControlTwo = false;
+        this.isEditableStepControlThree = true;
+        this.isEditableStepControlFour = false;
+        this.isEditableStepControlFive = false;
+        this.isCompletedtepControlOne = true;
+        this.isCompletedtepControlTwo = true;
+        this.isCompletedtepControlThree = false;
+        this.isCompletedtepControlFour = false;
+        this.isCompletedtepControlFive = false;
+        this.currentStepMat = 2;
 
-        } else if(this.tracingI.estado.estadoId === 5 ){
-          this.isEditableStepControlOne = false;
-          this.isEditableStepControlTwo = false;
-          this.isEditableStepControlThree = false;
-          this.isEditableStepControlFour = false;
-          this.isEditableStepControlFive = true;
-          this.isCompletedtepControlOne = true;
-          this.isCompletedtepControlTwo = true;
-          this.isCompletedtepControlThree = true;
-          this.isCompletedtepControlFour = true;
-          this.isCompletedtepControlFive = true;
-          this.currentStepMat = 4;
+      } else if (this.tracingI.estado.estadoId === 4) {
+        console.log("entre")
+        this.isEditableStepControlOne = false;
+        this.isEditableStepControlTwo = false;
+        this.isEditableStepControlThree = false;
+        this.isEditableStepControlFour = true;
+        this.isEditableStepControlFive = false;
+        this.isCompletedtepControlOne = true;
+        this.isCompletedtepControlTwo = true;
+        this.isCompletedtepControlThree = true;
+        this.isCompletedtepControlFour = false;
+        this.isCompletedtepControlFive = false;
+        this.currentStepMat = 3;
 
-        }
+      } else if (this.tracingI.estado.estadoId === 5) {
+        this.isEditableStepControlOne = false;
+        this.isEditableStepControlTwo = false;
+        this.isEditableStepControlThree = false;
+        this.isEditableStepControlFour = false;
+        this.isEditableStepControlFive = true;
+        this.isCompletedtepControlOne = true;
+        this.isCompletedtepControlTwo = true;
+        this.isCompletedtepControlThree = true;
+        this.isCompletedtepControlFour = true;
+        this.isCompletedtepControlFive = true;
+        this.currentStepMat = 4;
 
-        console.log("data", data.estado.estadoId);
-      },
+      }
+
+      console.log("data", data.estado.estadoId);
+      this.isLoading = false;
+    },
       err => {
-      /*   if (err.status === 401 || err.status === 403) {
-          this.openDialogInfo('Sesión caducada  !', 'Vuelva a iniciar sesión', 'error');
-          setTimeout (() => {
-                this.authService.logout();
-          }, 3500);
-        } */
-        console.log(err);
+        this.isLoading = false;
+        this.mostrarError = true;
       }
     );
   }
-  
-  
+
+
 }
 
